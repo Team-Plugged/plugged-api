89ab4f0eb133e5169d957efc05bafa2b544b91b0
passy4ucj <ojinnnakapascal@gmail.com>
2020-11-20 10:39:58 +0100

    Server JS - middleware import

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
+
+app.use(express.json())
 
 
 
@@ -17,4 +25,8 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-app.listen(5000, console.log('API running on PORT 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, 
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
