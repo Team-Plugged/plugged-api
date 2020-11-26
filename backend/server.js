038bec8afcf52f129b9ed65e1bccbe63047d5519
passy4ucj <ojinnnakapascal@gmail.com>
2020-11-26 10:58:50 +0100

    Upload Router

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -6,7 +7,7 @@ import connectDB from './config/db.js'
 import patientRoutes from './routes/patientRoutes.js'
 import hospitalRoutes from './routes/hospitalRoutes.js'
 import healthRecordRoutes from './routes/healthRecordRoutes.js'
-
+import uploadRoutes from './routes/uploadRoutes.js'
 
 
 
@@ -24,9 +25,14 @@ app.get('/api', (req, res) => {
     res.send('API is running')
 })
 
+const __dirname = path.resolve()
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.use('/api/patients', patientRoutes)
 app.use('/api/hospitals', hospitalRoutes)
 app.use('/api/healthrecords', healthRecordRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
